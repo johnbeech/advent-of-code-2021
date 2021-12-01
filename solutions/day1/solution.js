860e27057be7801bee9a54f571688a95e668fcbd
John Beech <johnbeech.github@mkv25.net>
2021-12-01 05:17:29 +0000

    Solve day1 for second star

--- solutions/day1/solution.js
+++ solutions/day1/solution.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { read, position } = require('promise-path')
+const { read, position, write } = require('promise-path')
 const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
 
@@ -10,7 +10,7 @@ async function run () {
   await solveForSecondStar(input)
 }
 
-async function solveForFirstStar (input) {
+function parseDepths (input) {
   const depths = input.split('\n').map(n => Number.parseInt(n)).map(n => {
     return {
       scan: n
@@ -22,6 +22,11 @@ async function solveForFirstStar (input) {
     depth.diff = depth.scan - previousDepth.scan
   })
 
+  return depths
+}
+
+async function solveForFirstStar (input) {
+  const depths = parseDepths(input)
   const solution = depths.filter(d => d.diff > 0).length - 1
 
   report('Input:', input)
@@ -29,7 +34,23 @@ async function solveForFirstStar (input) {
 }
 
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const depths = parseDepths(input)
+
+  depths.forEach((depth, index) => {
+    const p1 = depth
+    const p2 = depths[index - 1] || { scan: 0 }
+    const p3 = depths[index - 2] || { scan: 0 }
+    depth.window = p1.scan + p2.scan + p3.scan
+  })
+
+  depths.forEach((depth, index) => {
+    const previousDepth = depths[index - 1] || { scan: 0 }
+    depth.diff = depth.window - previousDepth.window
+  })
+
+  await write(fromHere('depths.json'), JSON.stringify(depths, null, 2), 'utf8')
+
+  const solution = depths.filter(d => d.diff > 0).length - 2
   report('Solution 2:', solution)
 }
 

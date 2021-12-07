4da1062bd55dd2b1c7cc14f422772efb4925a0b3
John Beech <johnbeech.github@mkv25.net>
2021-12-07 22:20:52 +0000

    Bind input text to make it more accessible

--- solutions/day5/Viewer.vue
+++ solutions/day5/Viewer.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
+    <h3>My Input</h3>
+    <shared-TextLoader file="input.txt" v-model="inputText" />
+
     <h2>My Solution</h2>
 
     <h3>Solution Code</h3>
     <shared-TextLoader file="solution.js" class="language-js" />
-
-    <h3>Input</h3>
-    <shared-TextLoader file="input.txt" />
 
     <h3>README</h3>
     <shared-TextLoader file="README.md" class="language-md" />
@@ -20,7 +20,6 @@
 export default {
   data: () => {
     return {
-      solutionText: '',
       inputText: ''
     }
   }

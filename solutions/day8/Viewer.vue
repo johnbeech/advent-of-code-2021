3183a47443d5dc07aabebc23c8b8a2a16ad64364
johnbeech <johnbeech@users.noreply.github.com>
2021-12-08 21:26:34 +0000

    deploy: cd791db7d7aaf45748b9e8cf01d27254644a1e77

--- solutions/day8/Viewer.vue
+++ solutions/day8/Viewer.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
     <h3>Seven Segment Display</h3>
-    <div class="display">
-      <div v-for="cell in cells" :class="`cell ${cell.char}`" :key="cell.key">{{ cell.char }}</div>
+
+    <day8-SegmentDisplay />
+    <day8-SegmentDisplay />
+    <day8-SegmentDisplay />
+    <day8-SegmentDisplay />
+
+    <h3>Puzzles</h3>
+
+    <p>Solution 1: <b>{{ solution1 }}</b></p>
+
+    <div v-for="puzzle in puzzles" :key="puzzle.codes.map(n => n.code).join('-')">
+      <div>
+        <span v-for="(item, index) in puzzle.codes" :key="`${item.code}-${index}`"
+          class="puzzle code">#{{ item.number || '?' }} : {{ item.code }}</span>
+      </div>
+      <div>
+        <span v-for="(item, index) in puzzle.output" :key="`${item.code}-${index}`"
+          class="puzzle output">#{{ item.number || '?' }} : {{ item.code }}</span>
+      </div>
     </div>
 
     <h3>My Input</h3>
@@ -22,100 +39,93 @@
 </template>
 
 <script>
+function parseInput(input) {
+  const lines = input.split('\n')
+    .map(n => n.trim())
+    .filter(n => n)
+  const puzzles = lines.map(line => parsePuzzleLine(line))
+  return puzzles
+}
 
-function cell(x, y, char) {
+function lookup(code) {
+  const numbersByCodeLength = {
+    2: 1,
+    4: 4,
+    3: 7,
+    7: 8
+  }
+
+  return numbersByCodeLength[code.length] || 0
+}
+
+function parsePuzzleLine(line) {
+  const [a, b] = line.split('|')
+  const codes = a.trim().split(' ')
+    .sort((a, b) => a.length > b.length ? -1 : 1)
+    .map(code => {
+      return {
+        code,
+        number: lookup(code)
+      }
+    })
+  const output = b.trim().split(' ')
+    .map(code => {
+      return {
+        code,
+        number: lookup(code)
+      }
+    })
   return {
-    x, y, char,
-    key: [x, y].join(':')
+    codes,
+    output
   }
 }
 
+function solveForFirstStar(puzzles) {
+  return puzzles.reduce((acc, puzzle) => {
+    const count = puzzle.output.reduce((po, item) => {
+      return po + (item.number ? 1 : 0)
+    }, 0)
+    return acc + count
+  }, 0)
+} 
+
 export default {
-  data: () => {
+  data() {
     return {
-      inputText: ''
+      inputText: '',
+      solution1: '?'
     }
   },
   computed: {
-    cells() {
-      return [
-        cell(0, 0, 'x'),
-        cell(1, 0, 'a'),
-        cell(2, 0, 'a'),
-        cell(3, 0, 'a'),
-        cell(4, 0, 'a'),
-        cell(0, 0, 'x'),
-        //
-        cell(0, 0, 'b'),
-        cell(1, 0, 'x'),
-        cell(2, 0, 'x'),
-        cell(3, 0, 'x'),
-        cell(4, 0, 'x'),
-        cell(0, 0, 'c'),
-        //
-        cell(0, 0, 'b'),
-        cell(1, 0, 'x'),
-        cell(2, 0, 'x'),
-        cell(3, 0, 'x'),
-        cell(4, 0, 'x'),
-        cell(0, 0, 'c'),
-        //
-        cell(0, 0, 'x'),
-        cell(1, 0, 'd'),
-        cell(2, 0, 'd'),
-        cell(3, 0, 'd'),
-        cell(4, 0, 'd'),
-        cell(0, 0, 'x'),
-        //
-        cell(0, 0, 'e'),
-        cell(1, 0, 'x'),
-        cell(2, 0, 'x'),
-        cell(3, 0, 'x'),
-        cell(4, 0, 'x'),
-        cell(0, 0, 'f'),
-        //
-        cell(0, 0, 'e'),
-        cell(1, 0, 'x'),
-        cell(2, 0, 'x'),
-        cell(3, 0, 'x'),
-        cell(4, 0, 'x'),
-        cell(0, 0, 'f'),
-        //
-        cell(0, 0, 'x'),
-        cell(1, 0, 'g'),
-        cell(2, 0, 'g'),
-        cell(3, 0, 'g'),
-        cell(4, 0, 'g'),
-        cell(0, 0, 'x'),
-      ]
-    },
-    cellMap() {
-      return this.cells.reduce((cellMap, cell) => {
-        cellMap[cell.key] = cell
-        return cellMap
-      }, {})
+    puzzles() {
+      const puzzles = parseInput(this.inputText)
+      this.solution1 = solveForFirstStar(puzzles)
+      return puzzles
     }
   }
 }
 </script>
 
 <style scoped>
-.display {
+.puzzle.code {
   display: inline-block;
-  overflow: hidden;
-  width: 120px;
-  height: 210px;
+  margin: 0.2em 0.2em;
+  padding: 0.1em 0.2em;
+  border: 0.1em;
+  background: #DDD;
+  border-radius: 0.2em;
+  color: black;
+  font-size: 0.8em;
 }
-.display > .cell {
-  display: inline-flex;
-  overflow: hidden;
-  width: 20px;
-  height: 30px;
-  align-content: center;
-  justify-content: center;
-  font-weight: bold;
-}
-.cell.x {
-  opacity: 0.1;
+.puzzle.output {
+  display: inline-block;
+  margin: 0.2em 0.2em;
+  padding: 0.1em 0.2em;
+  border: 0.1em;
+  background: #7CF;
+  border-radius: 0.2em;
+  color: black;
+  font-size: 0.8em;
 }
 </style>

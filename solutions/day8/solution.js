cd791db7d7aaf45748b9e8cf01d27254644a1e77
John Beech <johnbeech.github@mkv25.net>
2021-12-08 21:24:44 +0000

    Solve day 8 for first star

--- solutions/day8/solution.js
+++ solutions/day8/solution.js
@@ -10,8 +10,57 @@ async function run () {
   await solveForSecondStar(input)
 }
 
+function parseInput (input) {
+  const lines = input.split('\n')
+    .map(n => n.trim())
+    .filter(n => n)
+  const puzzles = lines.map(line => parsePuzzleLine(line))
+  return puzzles
+}
+
+function lookup (code) {
+  const numbersByCodeLength = {
+    2: 1,
+    4: 4,
+    3: 7,
+    7: 8
+  }
+
+  return numbersByCodeLength[code.length] || 0
+}
+
+function parsePuzzleLine (line) {
+  const [a, b] = line.split('|')
+  const codes = a.trim().split(' ')
+    .sort((a, b) => a.length > b.length ? -1 : 1)
+    .map(code => {
+      return {
+        code,
+        number: lookup(code)
+      }
+    })
+  const output = b.trim().split(' ')
+    .map(code => {
+      return {
+        code,
+        number: lookup(code)
+      }
+    })
+  return {
+    codes,
+    output
+  }
+}
+
 async function solveForFirstStar (input) {
-  const solution = 'UNSOLVED'
+  const puzzles = parseInput(input)
+  const solution = puzzles.reduce((acc, puzzle) => {
+    const count = puzzle.output.reduce((po, item) => {
+      return po + (item.number ? 1 : 0)
+    }, 0)
+    return acc + count
+  }, 0)
+
   report('Input:', input)
   report('Solution 1:', solution)
 }

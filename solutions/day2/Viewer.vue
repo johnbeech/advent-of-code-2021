3fb8989f3373e041cd0b3c3f9f0300b3982fafb2
John Beech <johnbeech.github@mkv25.net>
2021-12-12 19:44:28 +0000

    Animate sub diving into the ocean

--- solutions/day2/Viewer.vue
+++ solutions/day2/Viewer.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <shared-PanAndZoom class="submarine-tracker" :centerContent="true" v-on:ready="registerPanAndZoom">
+      <div v-for="(marker, idx) in markers" :key="`marker-${idx}`"
+        :style="markerStyle(marker)" class="marker">X</div>
+      <div class="submarine" :style="subStyle">
+        <label>SUB</label>
+      </div>
+    </shared-PanAndZoom>
+
     <h3>My Input</h3>
     <shared-TextLoader file="input.txt" v-model="inputText" />
 
@@ -17,15 +25,150 @@
 </template>
 
 <script>
+
+const part1Commands = {
+  forward,
+  down,
+  up
+}
+
+const part2Commands = {
+  forward,
+  down: aimDown,
+  up: aimUp
+}
+
+function parseInstructions (input) {
+  const lines = input.trim().split('\n')
+  const instructions = lines.map(line => {
+    const [direction, distance] = line.split(' ')
+    return {
+      direction,
+      distance: Number.parseInt(distance)
+    }
+  }).filter(n => n)
+  return instructions
+}
+
+function forward ({ distance }, position) {
+  position.x = position.x + distance
+  position.y = position.y + position.aim * distance
+}
+
+function down ({ distance }, position) {
+  position.y = position.y + distance
+}
+
+function up ({ distance }, position) {
+  position.y = position.y - distance
+}
+
+function aimDown ({ distance }, position) {
+  position.aim = position.aim + distance
+}
+
+function aimUp ({ distance }, position) {
+  position.aim = position.aim - distance
+}
+
 export default {
   data: () => {
     return {
-      inputText: ''
+      inputText: '',
+      sub: {
+        x: 0,
+        y: 0,
+        aim: 0,
+        rotation: 0,
+        wobble: 0
+      },
+      subStyle: '',
+      instructions: [],
+      markers: [{ x: 0, y: 0 }],
+      panAndZoom: false
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.advance()
+    }, 10)
+  },
+  methods: {
+    advance() {
+      const { sub, instructions } = this
+      const next = instructions.shift()
+      if (!next) {
+        return
+      }
+      const command = part2Commands[next.direction]
+      command(next, sub)
+      const markers = this.markers
+      markers.push({ x: sub.x, y: sub.y })
+      while (markers.length > 50) {
+        markers.shift()
+      }
+      this.computeSubstyle()
+    },
+    computeSubstyle() {
+      const { sub, panAndZoom } = this
+      sub.rotation = Math.cos(sub.x / sub.y * 0.01) * 180 / Math.PI
+      const style = `position: absolute; left: ${sub.x}px; top: ${sub.y / 100}px; transform: rotate(${sub.rotation + sub.wobble}deg)`
+      this.subStyle = style
+      if (panAndZoom) {
+        panAndZoom.moveTo(-sub.x, -sub.y / 100)
+      }
+    },
+    markerStyle(marker) {
+      const style = `position: absolute; left: ${marker.x}px; top: ${marker.y / 100}px;`
+      return style
+    },
+    registerPanAndZoom(panAndZoom) {
+      this.panAndZoom = panAndZoom
+    }
+  },
+  watch: {
+    inputText() {
+      const instructions = parseInstructions(this.inputText)
+      this.instructions = instructions
     }
   }
 }
 </script>
 
 <style scoped>
-
+.submarine-tracker {
+  height: 400px;
+  border: 1px solid #ccc;
+  background: cornflowerblue;
+}
+.submarine {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background: orange;
+  width: 60px;
+  height: 20px;
+  margin-left: -30px;
+  margin-top: -10px;
+  overflow: hidden;
+  text-align: center;
+}
+.submarine > label {
+  font-size: 10px;
+  font-weight: bold;
+}
+.marker {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background: rgba(255, 255, 255, 0.1);
+  width: 20px;
+  height: 20px;
+  margin-left: -10px;
+  margin-top: -10px;
+  overflow: hidden;
+  text-align: center;
+}
 </style>

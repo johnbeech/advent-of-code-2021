b8414292730dc4385719d462a853f6ff89572ce6
John Beech <johnbeech.github@mkv25.net>
2021-12-14 09:42:44 +0000

    Debug day 14 using test input

---
+++ solutions/day14/Viewer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>My Input</h3>
+    <shared-TextLoader file="input.txt" v-model="inputText" />
+
+    <h2>My Solution</h2>
+
+    <h3>Solution Code</h3>
+    <shared-TextLoader file="solution.js" class="language-js" />
+
+    <h3>README</h3>
+    <shared-TextLoader file="README.md" class="language-md" />
+
+    <h3>Viewer</h3>
+    <shared-TextLoader file="Viewer.vue" class="language-markup" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      inputText: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ solutions/day14/solution.js
@@ -0,0 +1,91 @@
+const path = require('path')
+const { read, position } = require('promise-path')
+const fromHere = position(__dirname)
+const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
+
+async function run () {
+  const input = (await read(fromHere('test.txt'), 'utf8')).trim()
+
+  await solveForFirstStar(input)
+  await solveForSecondStar(input)
+}
+
+function parsePolymerInstructions (input) {
+  const lines = input.split('\n').map(n => n.trim()).filter(n => n)
+  const polymerTemplate = lines.shift().split('')
+
+  const pairInsertionRules = lines.map(line => {
+    const [left, c] = line.split(' -> ')
+    const [a, b] = left.split('')
+    return {
+      a, b, c
+    }
+  })
+
+  return {
+    polymerTemplate,
+    pairInsertionRules
+  }
+}
+
+function splitPolymers (instructions) {
+  const { polymerTemplate, pairInsertionRules } = instructions
+  const template = polymerTemplate
+  const rules = pairInsertionRules
+  const newTemplate = []
+
+  template.forEach((k, i) => {
+    newTemplate.push(k)
+    const next = template[i + 1]
+    if (next) {
+      const rule = rules.filter(n => n.a === k && n.b === next)[0]
+      newTemplate.push(rule.c)
+    }
+  })
+
+  return {
+    polymerTemplate: newTemplate,
+    pairInsertionRules: rules
+  }
+}
+
+function findLowestAndHighest (list) {
+  const map = list.reduce((acc, item) => {
+    acc[item] = acc[item] ? acc[item] + 1 : 1
+    return acc
+  }, {})
+
+  const sorted = Object.entries(map).sort((a, b) => a[1] < b[1] ? -1 : 1)
+  const lowest = sorted[0]
+  const highest = sorted[sorted.length - 1]
+  console.log('Values:', map)
+  return {
+    lowest,
+    highest
+  }
+}
+
+async function solveForFirstStar (input) {
+  const instructions = parsePolymerInstructions(input)
+  report('Instructions:', instructions)
+
+  let split = instructions
+  const splits = [split]
+  while (splits.length <= 10) {
+    split = splitPolymers(split)
+    splits.push(split)
+    console.log(split.polymerTemplate.join(''))
+  }
+
+  const lastSplit = split
+  const { lowest, highest } = findLowestAndHighest(lastSplit.polymerTemplate)
+  const solution = highest[1] - lowest[1]
+  report('Solution 1:', solution)
+}
+
+async function solveForSecondStar (input) {
+  const solution = 'UNSOLVED'
+  report('Solution 2:', solution)
+}
+
+run()
